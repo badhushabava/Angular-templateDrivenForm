baaa31b98af603c6c1e53648cdc03186fcbac7f6
badhushanb <badhushanb@gmail.com>
2023-04-20 14:35:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular-TemplateDrivenForm';
+  title = 'Template-Driven Form';
+  isTrue: boolean = true;
+  name1: any;
+  email1: any;
+  gender1: any;
+
+  submit(form: NgForm) {
+    console.log(form.value);
+    this.name1 = form.value.name;
+    this.email1 = form.value.email;
+    this.gender1 = form.value.gender;
+    this.isTrue = false;
+  }
 }
